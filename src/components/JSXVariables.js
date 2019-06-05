c107d08f790ebcac98f9b06a284a92a4838ca552
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2019-06-04 20:27:47 -0500

    I just added inline styles changed the apperance of something using inline styles

--- src/components/JSXVariables.js
+++ src/components/JSXVariables.js
@@ -1,14 +1,27 @@
 import React from "react";
-const name = 'john morgan broaddus IV';
+import './helloDiv.css'
+const name = "john morgan broaddus IV";
+var styles = {
+  menu: {
+    color: "red",
+    backgroundColor: "yellow"
+},
+menuButton: {
+  color: "yellow",
+  backgroundColor: "green"
+}
+}
 
 function JSXVariables() {
   return (
     <div className="main-container">
       <div className="container">
         <div className="jumbotron">
-          <h1>Hi! My name is {name}</h1>
-          <h2>My name has {name.length}  letters</h2>
-          <h2>I think React {'wowowowowowowowowowowo'}</h2>
+          <h1 style={styles.menuButton}>
+            Hi! My name is {name}
+          </h1>
+          <h2>My name has {name.length} letters</h2>
+          <h2>I think React {"wowowowowowowowowowowo"}</h2>
         </div>
       </div>
     </div>

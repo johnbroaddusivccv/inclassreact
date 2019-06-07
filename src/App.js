c246a725a69c630164cf8e0ba5d4c80864fdfbba
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2019-06-06 19:59:21 -0500

    card and mapping through json and displaying it to the DOM

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
-import JSXVariables from "./components/JSXVariables";
-
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Card from "./components/Card"
+import friends from "./friends.json"
 function App() {
-  return <JSXVariables />;
+
+  console.log(friends)
+  return (
+    <Wrapper>
+      <Title>Friends List</Title>
+      {friends.map((friend, i) => <Card key={i} name={friend.name} occupation={friend.occupation} location={friend.location} src={friend.image} />)}
+    </Wrapper>
+  );
 }
 
 export default App;
